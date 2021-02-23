44f21c2712660df9388226af4542fb30acf479c5
Guilherme Araujo <guilherme.araujo@imd.ufrn.br>
2021-02-23 20:50:58 -0300

    Added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express'
+import cors from 'cors'
 import router from './routes'
 import bodyParser from 'body-parser'
 
 const app = express()
 
+app.use(cors)
 app.use(bodyParser.json())
-
 app.use(router)
 
 export default app
