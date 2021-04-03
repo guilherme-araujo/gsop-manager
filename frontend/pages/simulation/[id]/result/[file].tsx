55134ec2fb3bf09b08fcd4f84c8c37ee7c6c8c7d
Guilherme Araújo <guilherme.araujo@imd.ufrn.br>
2021-04-03 20:56:35 -0300

    Fix image loading from results
    - Had to revert to a simple image instead of next/image

--- frontend/pages/simulation/[id]/result/[file].tsx
+++ frontend/pages/simulation/[id]/result/[file].tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
 import Layout from '../../../../components/Layout'
 
@@ -13,12 +12,10 @@ const Program = () => {
         <>
           <Link href={`/simulation/${id}`}>Back to simulation</Link>
           <h1>Result image {file}</h1>
-          <Image
-            src={`http://localhost:3000/api/resultdownload/${id}/${file}`}
-            alt={`${file}`}
-            width={1200}
-            height={800}
-          ></Image>
+
+          <img
+            src={`http://localhost:3001/gsop-admin-web/api/resultdownload/${id}/${file}`}
+          />
         </>
       ) : (
         <p>Loading...</p>
